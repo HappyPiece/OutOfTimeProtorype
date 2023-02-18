ad0d802990760e89f23d0b8c5d9841eac6e079a1
vladovello <vladsuhankov@gmail.com>
2023-02-18 22:49:06 +0700

    feat(AutoMapper): Added extension method to ignore specific field (not tested)

--- OutOfTimePrototype/Config/MappingProfile.cs
+++ OutOfTimePrototype/Config/MappingProfile.cs
@@ -1,4 +1,6 @@
+using System.Linq.Expressions;
 using AutoMapper;
+using LanguageExt.ClassInstances;
 using OutOfTimePrototype.DAL.Models;
 using OutOfTimePrototype.DTO;
 
@@ -8,17 +10,27 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<LectureHallDto, LectureHall>().IgnoreNullProperties();
-        CreateMap<LectureHallUpdateModel, LectureHall>().IgnoreNullProperties();
+        CreateMap<LectureHallDto, LectureHall>().IgnoreNullProperties().IgnoreProperty(dto => dto.Id);
+        CreateMap<LectureHallUpdateDto, LectureHall>().IgnoreNullProperties();
     }
 }
 
 public static class MappingExpressionExtension
 {
-    public static void IgnoreNullProperties<TDestination, TSource>(
-        this IMappingExpression<TDestination, TSource> expression)
+    public static IMappingExpression<TSource, TDestination> IgnoreNullProperties<TSource, TDestination>(
+        this IMappingExpression<TSource, TDestination> expression
+    )
     {
         expression.ForAllMembers(options =>
             options.Condition((_, _, srcMember) => srcMember != null));
+        return expression;
+    }
+
+    public static IMappingExpression<TSource, TDestination> IgnoreProperty<TSource, TDestination>(
+        this IMappingExpression<TSource, TDestination> expression, Expression<Func<TSource, object>> getProp
+    )
+    {
+        expression.ForSourceMember(getProp, configurationExpression => configurationExpression.DoNotValidate());
+        return expression;
     }
 }
