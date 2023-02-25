b3f8cbe57f98501c68d7655e10d08a74fa7d4e49
vladovello <vladsuhankov@gmail.com>
2023-02-25 19:14:41 +0700

    :sparkles: feat(AutoMapper): Added new mapping profiles

--- OutOfTimePrototype/Configurations/MappingProfile.cs
+++ OutOfTimePrototype/Configurations/MappingProfile.cs
@@ -1,21 +1,39 @@
 using System.Linq.Expressions;
+using System.Xml;
 using AutoMapper;
-using LanguageExt.ClassInstances;
 using OutOfTimePrototype.Dal.Models;
 using OutOfTimePrototype.DAL.Models;
 using OutOfTimePrototype.Dto;
 using OutOfTimePrototype.DTO;
 
-namespace OutOfTimePrototype.Config;
+namespace OutOfTimePrototype.Configurations;
 
 public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<LectureHallDto, LectureHall>().IgnoreNullProperties().IgnoreProperty(dto => dto.Id);
-        CreateMap<LectureHallUpdateDto, LectureHall>().IgnoreNullProperties();
-        CreateMap<EducatorDto, Educator>().IgnoreNullProperties().IgnoreProperty(dto => dto.Id);
+        // Disable Null
+        CreateMap<int?, int>().ConvertUsing((src, dest) => src ?? dest);
+        CreateMap<string?, string>().ConvertUsing((src, dest) => src ?? dest);
+        CreateMap<DateTime?, DateTime>().ConvertUsing((src, dest) => src ?? dest);
+        CreateMap<TimeSpan?, TimeSpan>().ConvertUsing((src, dest) => src ?? dest);
+        CreateMap<Guid?, Guid>().ConvertUsing((src, dest) => src ?? dest);
+
+        // Campus building
         CreateMap<CampusBuildingDto, CampusBuilding>().IgnoreNullProperties().IgnoreProperty(dto => dto.Id);
+
+        // Lecture hall
+        CreateMap<LectureHallDto, LectureHall>().IgnoreNullProperties().IgnoreProperty(dto => dto.Id);
+        // .ForMember(hall => hall.HostBuilding, opts => opts.MapFrom((dto, hall) =>
+        // {
+        //     if (hall.HostBuilding is null) return new CampusBuilding { Id = dto.HostBuildingId };
+        //
+        //     hall.HostBuilding.Id = dto.HostBuildingId;
+        //     return hall.HostBuilding;
+        // }));
+        CreateMap<LectureHallUpdateDto, LectureHall>().IgnoreNullProperties();
+
+        CreateMap<EducatorDto, Educator>().IgnoreNullProperties().IgnoreProperty(dto => dto.Id);
         CreateMap<UserDto, User>().IgnoreNullProperties().IgnoreProperty(dto => dto.Id);
     }
 }
