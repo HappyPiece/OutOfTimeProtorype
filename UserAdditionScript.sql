44984879fefbf7b97d1cf22232e0c3c148f3e218
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2023-02-28 19:50:15 +0700

    Add RootAdditionScript

--- UserAdditionScript.sql
+++
@@ -1,2 +0,0 @@
-Insert into "Users" ("Id", "Email", "Password", "AccountType", "VerifiedRoles", "ClaimedRoles") 
-values ('00000000-0000-0000-0000-000000000000', 'root@root.net', 'aboba', 0, ARRAY[0]::integer[], Array[]::integer[]);
---
+++ sql/RootAdditionScript.sql
@@ -0,0 +1,2 @@
+Insert into "Users" ("Id", "Email", "Password", "AccountType", "VerifiedRoles", "ClaimedRoles") 
+values ('00000000-0000-0000-0000-000000000000', :'ROOT_EMAIL', :'ROOT_PASS', 0, ARRAY[0]::integer[], Array[]::integer[]);
