e919f214de29cf359cf7db6c8b011a1dc8ce177b
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2023-03-01 21:46:49 +0700

    Add Gitlab CI pipeline config

--- sql/RootAdditionScript.sql
+++ sql/RootAdditionScript.sql
@@ -1,2 +1,3 @@
 Insert into "Users" ("Id", "Email", "Password", "AccountType", "VerifiedRoles", "ClaimedRoles") 
-values ('00000000-0000-0000-0000-000000000000', :'ROOT_EMAIL', :'ROOT_PASS', 0, ARRAY[0]::integer[], Array[]::integer[]);
+values ('00000000-0000-0000-0000-000000000000', :'ROOT_EMAIL', :'ROOT_PASS', 0, ARRAY[0]::integer[], Array[]::integer[])
+ON CONFLICT DO UPDATE;
