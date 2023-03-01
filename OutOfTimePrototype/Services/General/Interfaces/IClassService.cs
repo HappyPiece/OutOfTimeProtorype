4b8e0bd97dba3e518edd61d6ee9762c0d8f3272d
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2023-03-01 14:33:05 +0700

    Add requests for time slots and class types

--- OutOfTimePrototype/Services/General/Interfaces/IClassService.cs
+++ OutOfTimePrototype/Services/General/Interfaces/IClassService.cs
@@ -7,8 +7,8 @@ namespace OutOfTimePrototype.Services.Interfaces
 {
     public interface IClassService
     {
-        Task<ClassOperationResult> TryCreateClass(CreateClassDto createClassDto);
-        Task<ClassOperationResult> TryCreateClasses(ClassQueryDto classQueryDto, CreateClassDto createClassDto);
+        Task<ClassOperationResult> TryCreateClass(ClassCreateDto ClassCreateDto);
+        Task<ClassOperationResult> TryCreateClasses(ClassQueryDto classQueryDto, ClassCreateDto ClassCreateDto);
         Task<ClassOperationResult> TryEditClass(Guid id, ClassEditDto classEditDto, bool nullMode);
         Task<ClassOperationResult> TryEditClasses(ClassQueryDto classQueryDto, ClassEditDto classEditDto, bool nullMode);
         Task<ClassOperationResult> TryDeleteClass(Guid id);
